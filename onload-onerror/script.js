70c67929838d313e4fce4601c5afc1b43a2a7080
Andrei <an.cayman@ya.ru>
2022-02-23 16:10:03 +0200

    feat: refactor js

--- onload-onerror/script.js
+++ onload-onerror/script.js
@@ -5,7 +5,7 @@ const inputUrl = document.querySelector('#item-url');
 const formMessage = document.querySelector('.form-message');
 const img = document.createElement('img');
 
-function isImgURLValid(path) {
+function checkLinkForImg(path) {
   return new Promise((resolve, reject) => {
     img.src = path;
     img.onload = resolve;
@@ -17,7 +17,7 @@ function isImgURLValid(path) {
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   
-  isImgURLValid(inputUrl.value)
+  checkLinkForImg(inputUrl.value)
     .then(() => formMessage.textContent = `Path is valid`)
     .catch(() => formMessage.textContent = `Path isn't valid`)
 })
