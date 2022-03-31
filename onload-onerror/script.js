66f2ac0f629bc5bc6be9da1581763dfafb3ff428
Andrei <an.cayman@ya.ru>
2022-03-31 18:54:21 +0200

    fix: promise

--- onload-onerror/script.js
+++ onload-onerror/script.js
@@ -1,23 +1,25 @@
-'use strict'
+"use strict";
 
-const form = document.querySelector('.form');
-const inputUrl = document.querySelector('#item-url');
-const formMessage = document.querySelector('.form-message');
-const img = document.createElement('img');
+const form = document.querySelector(".form");
+const inputUrl = document.querySelector("#item-url");
+const formMessage = document.querySelector(".form-message");
+const img = document.createElement("img");
 
 function checkLinkForImg(path) {
   return new Promise((resolve, reject) => {
     img.src = path;
     img.onload = resolve;
     img.onerror = reject;
-    form.after(img);
   });
-};
+}
 
-form.addEventListener('submit', function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
-  
+
   checkLinkForImg(inputUrl.value)
-    .then(() => formMessage.textContent = `Path is valid`)
-    .catch(() => formMessage.textContent = `Path isn't valid`)
-})
+    .then(() => {
+      formMessage.textContent = `Path is valid`;
+      form.after(img);
+    })
+    .catch(() => (formMessage.textContent = `Path isn't valid`));
+});
